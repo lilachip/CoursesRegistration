02d2df28b7baed387d347b3dab0ef338550a32cc
lilach <lilach.karavani@gmail.com>
2018-01-09 17:56:09 +0200

    second commit

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
-(function(angular) {
+(function (angular) {
     'use strict';
-var app = angular.module('app', []);
-app.controller('appController', function ($scope) {
-    $scope.courses = [{
-        id: 1,
-        student_firstName: "first student_firstName",
-        student_lastName: "first student_lastName",
-        course: "first course"
-    },
+    var app = angular.module('app', []);
+    app.controller('appController', ["$scope", function ($scope) {
+        $scope.courses = [{
+            id: 1,
+            student_firstName: "first student_firstName",
+            student_lastName: "first student_lastName",
+            course: "first course"
+        },
         {
             id: 2,
             student_firstName: "second student_firstName",
-        student_lastName: "second student_lastName",
-        course: "second course"
+            student_lastName: "second student_lastName",
+            course: "second course"
         },
         {
             id: 3,
             student_firstName: "third student_firstName",
-        student_lastName: "third student_lastName",
-        course: "third course"
+            student_lastName: "third student_lastName",
+            course: "third course"
         },
         {
             id: 4,
-            student_name: "fourth student_firstName",
-        student_lastName: "fourth student_lastName",
-        course: "fourth course"
+            student_firstName: "fourth student_firstName",
+            student_lastName: "fourth student_lastName",
+            course: "fourth course"
         }
-    ];
-    $scope.newarray = [];
-    $scope.sub = function ()
-    {
-        angular.forEach($scope.abc, function (value, key) {
-            $scope.newarray.push({ id: value.id, name: value.name, sal: value.sal, mnth: value.mnth, total: parseInt(value.sal) * parseInt(value.mnth) });
-        });
-    }
-    // $scope.Addcourse = function(course) {
-    //     if (course) {
-    //       var obj = {
-    //         id: id,
-    //         student_firstName:$scope.firstNameText,
-    //         student_lastName:$scope.lastNameText,
-    //         course: course
-    //       };
-    //    //   $scope.items.push($scope.input);
-    //       $scope.courses.push(obj);
-    //       this.id ='';
-    //       this.firstNameText = '';
-    //       this.lastNameText = '';
-    //       this.course ='';
-    //     }
-    //     console.log($scope.courses);
-    //   }
-});
+        ];    
+      console.log($scope.courses);
+      var count = $scope.courses.length;
+      console.log(count);
+      $scope.Addcourse = function(){ 
+          if($scope.firstNameText == null || $scope.lastNameText == null ||  $scope.selectedCourse ==null)
+          {           
+              alert("pls insert name and last name and the wanted course...");
+              return;
+          }
+    $scope.courses.push({
+        id:count+1,
+        student_firstName :$scope.firstNameText,
+        student_lastName :$scope.lastNameText,
+        course: $scope.selectedCourse.course
+    });
+    $scope.firstNameText =null;
+    $scope.lastNameText =null;
+      console.log($scope.courses);
+      };
+    }]);
 })(window.angular);
