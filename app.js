8c1eaed82509b136a56efb156638b14750705d10
lilach <lilach.karavani@gmail.com>
2018-01-11 13:42:59 +0200

    last commit

--- app.js
+++ app.js
@@ -1,50 +1,41 @@
-(function (angular) {
-    'use strict';
-    var app = angular.module('app', []);
-    app.controller('appController', ["$scope", function ($scope) {
-        $scope.courses = [{
-            id: 1,
-            student_firstName: "first student_firstName",
-            student_lastName: "first student_lastName",
-            course: "first course"
-        },
-        {
-            id: 2,
-            student_firstName: "second student_firstName",
-            student_lastName: "second student_lastName",
-            course: "second course"
-        },
-        {
-            id: 3,
-            student_firstName: "third student_firstName",
-            student_lastName: "third student_lastName",
-            course: "third course"
-        },
-        {
-            id: 4,
-            student_firstName: "fourth student_firstName",
-            student_lastName: "fourth student_lastName",
-            course: "fourth course"
-        }
-        ];    
-      console.log($scope.courses);
-      var count = $scope.courses.length;
-      console.log(count);
-      $scope.Addcourse = function(){ 
-          if($scope.firstNameText == null || $scope.lastNameText == null ||  $scope.selectedCourse ==null)
-          {           
-              alert("pls insert name and last name and the wanted course...");
-              return;
-          }
-    $scope.courses.push({
-        id:count+1,
-        student_firstName :$scope.firstNameText,
-        student_lastName :$scope.lastNameText,
-        course: $scope.selectedCourse.course
+
+var app = angular.module("app", []);
+
+app.service('dataService', function ($http) {
+    delete $http.defaults.headers.common['X-Requested-With'];
+    this.getData = function () {
+        return $http({
+            method: 'GET',
+            url: 'http://www.mocky.io/v2/5a563d0a2f00006e36beed34',
+            params: 'limit=10, sort_by=created:desc',
+            headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
+        });
+    }
+});
+
+app.controller('AngularJSCtrl', function ($scope, dataService) {
+    $scope.data = null;
+    $scope.coursesArray = [];
+    $scope.coursesRegistrationList = [];
+    dataService.getData().then(function (dataResponse) {
+        $scope.data = dataResponse;
+        $scope.coursesArray = dataResponse.data.courses;
+        $scope.Addcourse = function () {
+            if ($scope.firstNameText == null || $scope.lastNameText == null || $scope.selectedCourse == null) {
+                alert("pls insert name and last name and the wanted course...");
+                return;
+            }
+            var count = 0
+            $scope.coursesRegistrationList.push({
+                id: count + 1,
+                student_firstName: $scope.firstNameText,
+                student_lastName: $scope.lastNameText,
+                course: $scope.selectedCourse
+            });
+            $scope.firstNameText = null;
+            $scope.lastNameText = null;
+            console.log($scope.coursesRegistrationList);
+        };
+        console.log($scope.coursesArray);
     });
-    $scope.firstNameText =null;
-    $scope.lastNameText =null;
-      console.log($scope.courses);
-      };
-    }]);
-})(window.angular);
+});
