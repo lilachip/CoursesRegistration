230a258695520ca3918849d103701731aacd25b9
lilach <lilach.karavani@gmail.com>
2018-01-11 15:22:15 +0200

    last commit

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-
 var app = angular.module("app", []);
 
-app.service('dataService', function ($http) {
-    delete $http.defaults.headers.common['X-Requested-With'];
+app.service("dataService", function ($http) {
     this.getData = function () {
-        return $http({
-            method: 'GET',
-            url: 'http://www.mocky.io/v2/5a563d0a2f00006e36beed34',
-            params: 'limit=10, sort_by=created:desc',
-            headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
-        });
-    }
+        return $http.get("http://www.mocky.io/v2/5a563d0a2f00006e36beed34");
+    };
 });
 
-app.controller('AngularJSCtrl', function ($scope, dataService) {
+app.controller("AngularJSCtrl", function ($scope, dataService) {
     $scope.data = null;
     $scope.coursesArray = [];
     $scope.coursesRegistrationList = [];
+
     dataService.getData().then(function (dataResponse) {
         $scope.data = dataResponse;
         $scope.coursesArray = dataResponse.data.courses;
-        $scope.Addcourse = function () {
-            if ($scope.firstNameText == null || $scope.lastNameText == null || $scope.selectedCourse == null) {
-                alert("pls insert name and last name and the wanted course...");
-                return;
-            }
-            var count = 0
-            $scope.coursesRegistrationList.push({
-                id: count + 1,
-                student_firstName: $scope.firstNameText,
-                student_lastName: $scope.lastNameText,
-                course: $scope.selectedCourse
-            });
-            $scope.firstNameText = null;
-            $scope.lastNameText = null;
-            console.log($scope.coursesRegistrationList);
-        };
-        console.log($scope.coursesArray);
     });
+
+    var count = 0;
+    $scope.Addcourse = function () {
+        if ($scope.firstNameText == null || $scope.lastNameText == null || $scope.selectedCourse == null) {
+            alert("Please fill out all the fields before submitting");
+            return;
+        }
+
+        count++;
+        $scope.coursesRegistrationList.push({
+            id: count,
+            student_firstName: $scope.firstNameText,
+            student_lastName: $scope.lastNameText,
+            course: $scope.selectedCourse
+        });
+
+        $scope.firstNameText = null;
+        $scope.lastNameText = null;
+        $scope.selectedCourse = null;
+    };
 });
